b38c250f642577488db134d86f04a527ab0bc8a6
DudePascalou <dude.pascalou@gmail.com>
2025-12-25 23:38:38 +0100

    Migrate to .NET 8.0.

--- Clairvoyance.Services/MtgAbilityService.cs
+++ Clairvoyance.Services/MtgAbilityService.cs
@@ -3,66 +3,64 @@ using Clairvoyance.Domain.Abilities;
 using Clairvoyance.Domain.Conditions;
 using Clairvoyance.Domain.Costs;
 using Clairvoyance.Domain.Effects;
-using System.Collections.Generic;
 using System.Diagnostics;
 
-namespace Clairvoyance.Services
+namespace Clairvoyance.Services;
+
+public class MtgAbilityService
 {
-    public class MtgAbilityService
+    private readonly IDictionary<string, ICollection<IAbility>> _CardAbilitiesByName;
+
+    public MtgAbilityService()
     {
-        private readonly IDictionary<string, ICollection<IAbility>> _CardAbilitiesByName;
+        _CardAbilitiesByName = new Dictionary<string, ICollection<IAbility>>();
 
-        public MtgAbilityService()
+        // TODO : serialize/deserialize abilities on a json file.
+
+        AddAbility("Island", new ManaActivatedAbility(new IsOnBattlefieldCondition(), new TapCost(), new AddToManaPoolEffect("{U}")));
+        AddAbility("Forest", new ManaActivatedAbility(new IsOnBattlefieldCondition(), new TapCost(), new AddToManaPoolEffect("{G}")));
+
+        AddAbility("Elvish Spirit Guide", new ManaActivatedAbility(new IsInHandCondition(), new RemoveFromTheGameCost(), new AddToManaPoolEffect("{G}")));
+        AddAbility("Magus of the Library", new ManaActivatedAbility(new AndCondition(new IsOnBattlefieldCondition(), new CanTapCondition()), new TapCost(), new AddToManaPoolEffect("{C}")));
+        AddAbility("Llanowar Elves", new ManaActivatedAbility(new AndCondition(new IsOnBattlefieldCondition(), new CanTapCondition()), new TapCost(), new AddToManaPoolEffect("{G}")));
+        AddAbility("Hedron Archive", new ManaActivatedAbility(new IsOnBattlefieldCondition(), new TapCost(), new AddToManaPoolEffect("{C}{C}")));
+        AddAbility("Lotus Petal", new ManaActivatedAbility(new IsOnBattlefieldCondition(), new TapCost(), new AddToManaPoolEffect("{AC}")));
+        AddAbility("Ghost Quarter", new ManaActivatedAbility(new IsOnBattlefieldCondition(), new TapCost(), new AddToManaPoolEffect("{C}")));
+        AddAbility("Mishra's Factory", new ManaActivatedAbility(new IsOnBattlefieldCondition(), new TapCost(), new AddToManaPoolEffect("{C}")));
+        AddAbility("Spawning Bed", new ManaActivatedAbility(new IsOnBattlefieldCondition(), new TapCost(), new AddToManaPoolEffect("{C}")));
+        AddAbility("Unclaimed Territory", new ManaActivatedAbility(new IsOnBattlefieldCondition(), new TapCost(), new AddToManaPoolEffect("{C}"))); // TODO : choose a creature type
+    }
+
+    public void AddAbility(string cardName, params IAbility[] abilities)
+    {
+        if (!_CardAbilitiesByName.ContainsKey(cardName))
         {
-            _CardAbilitiesByName = new Dictionary<string, ICollection<IAbility>>();
-
-            // TODO : serialize/deserialize abilities on a json file.
-
-            AddAbility("Island", new ManaActivatedAbility(new IsOnBattlefieldCondition(), new TapCost(), new AddToManaPoolEffect("{U}")));
-            AddAbility("Forest", new ManaActivatedAbility(new IsOnBattlefieldCondition(), new TapCost(), new AddToManaPoolEffect("{G}")));
-
-            AddAbility("Elvish Spirit Guide", new ManaActivatedAbility(new IsInHandCondition(), new RemoveFromTheGameCost(), new AddToManaPoolEffect("{G}")));
-            AddAbility("Magus of the Library", new ManaActivatedAbility(new AndCondition(new IsOnBattlefieldCondition(), new CanTapCondition()), new TapCost(), new AddToManaPoolEffect("{C}")));
-            AddAbility("Llanowar Elves", new ManaActivatedAbility(new AndCondition(new IsOnBattlefieldCondition(), new CanTapCondition()), new TapCost(), new AddToManaPoolEffect("{G}")));
-            AddAbility("Hedron Archive", new ManaActivatedAbility(new IsOnBattlefieldCondition(), new TapCost(), new AddToManaPoolEffect("{C}{C}")));
-            AddAbility("Lotus Petal", new ManaActivatedAbility(new IsOnBattlefieldCondition(), new TapCost(), new AddToManaPoolEffect("{AC}")));
-            AddAbility("Ghost Quarter", new ManaActivatedAbility(new IsOnBattlefieldCondition(), new TapCost(), new AddToManaPoolEffect("{C}")));
-            AddAbility("Mishra's Factory", new ManaActivatedAbility(new IsOnBattlefieldCondition(), new TapCost(), new AddToManaPoolEffect("{C}")));
-            AddAbility("Spawning Bed", new ManaActivatedAbility(new IsOnBattlefieldCondition(), new TapCost(), new AddToManaPoolEffect("{C}")));
-            AddAbility("Unclaimed Territory", new ManaActivatedAbility(new IsOnBattlefieldCondition(), new TapCost(), new AddToManaPoolEffect("{C}"))); // TODO : choose a creature type
+            _CardAbilitiesByName.Add(cardName, new List<IAbility>());
         }
-
-        public void AddAbility(string cardName, params IAbility[] abilities)
+        foreach (var ability in abilities)
         {
-            if (!_CardAbilitiesByName.ContainsKey(cardName))
-            {
-                _CardAbilitiesByName.Add(cardName, new List<IAbility>());
-            }
+            _CardAbilitiesByName[cardName].Add(ability);
+        }
+    }
+
+    public void AffectAbilities(Card card)
+    {
+        if (_CardAbilitiesByName.ContainsKey(card.Name))
+        {
+            var abilities = _CardAbilitiesByName[card.Name].Clone(); // Abilities have to be cloned because they receive the card instance below.
             foreach (var ability in abilities)
             {
-                _CardAbilitiesByName[cardName].Add(ability);
+                card.Abilities.Add(ability);
+                ability.Card = card;
             }
         }
-
-        public void AffectAbilities(Card card)
+        else if (card.Abilities == null)
         {
-            if (_CardAbilitiesByName.ContainsKey(card.Name))
-            {
-                var abilities = _CardAbilitiesByName[card.Name].Clone(); // Abilities have to be cloned because they receive the card instance below.
-                foreach (var ability in abilities)
-                {
-                    card.Abilities.Add(ability);
-                    ability.Card = card;
-                }
-            }
-            else if (card.Abilities == null)
-            {
-                card.Abilities = new List<IAbility>();
-            }
-            else
-            {
-                Debug.WriteLine($"No ability found for {card.Name}.");
-            }
+            card.Abilities = new List<IAbility>();
+        }
+        else
+        {
+            Debug.WriteLine($"No ability found for {card.Name}.");
         }
     }
 }
--- Clairvoyance.Services/MtgStatisticsService.cs
+++ Clairvoyance.Services/MtgStatisticsService.cs
@@ -1,39 +1,37 @@
 ﻿using Clairvoyance.Domain;
 using Clairvoyance.Domain.Statistics;
-using System.Linq;
 
-namespace Clairvoyance.Services
+namespace Clairvoyance.Services;
+
+public class MtgStatisticsService
 {
-    public class MtgStatisticsService
+    public MtgStatisticsService()
+    { }
+
+    /// <summary>
+    /// Compute statistics of what cards can be played within <paramref name="turnCount"/> turns
+    /// on a goldfish mode (considering opponent is doing nothing).
+    /// TODO : statistics are computed among a large number of cases.
+    /// </summary>
+    /// <param name="turnCount">Turns count among which compute statistics.</param>
+    public DeckPlayabilityStatistics ComputePlayabilityStatistics(Deck deck, Player player, int turnCount)
     {
-        public MtgStatisticsService()
-        { }
+        var stats = new DeckPlayabilityStatistics(deck, turnCount);
+        var gameStats = stats.AddGame();
 
-        /// <summary>
-        /// Compute statistics of what cards can be played within <paramref name="turnCount"/> turns
-        /// on a goldfish mode (considering opponent is doing nothing).
-        /// TODO : statistics are computed among a large number of cases.
-        /// </summary>
-        /// <param name="turnCount">Turns count among which compute statistics.</param>
-        public DeckPlayabilityStatistics ComputePlayabilityStatistics(Deck deck, Player player, int turnCount)
+        player.ShuffleLibrary();
+        player.Draw(7);
+        gameStats.AddStartingHand(player.Hand);
+
+        for (int turn = 1; turn <= turnCount; turn++)
         {
-            var stats = new DeckPlayabilityStatistics(deck, turnCount);
-            var gameStats = stats.AddGame();
+            player.TakeTurn(turn, turn == 1);
 
-            player.ShuffleLibrary();
-            player.Draw(7);
-            gameStats.AddStartingHand(player.Hand);
-
-            for (int turn = 1; turn <= turnCount; turn++)
-            {
-                player.TakeTurn(turn, turn == 1);
-
-                var turnStats = gameStats.AddNewTurn();
-                turnStats.Creatures = player.Battlefield.Creatures().ToList();
-                turnStats.Lands = player.Battlefield.Lands().ToList();
-            }
-
-            return stats;
+            var turnStats = gameStats.AddNewTurn();
+            turnStats.Creatures = player.Battlefield.Creatures().ToList();
+            turnStats.Lands = player.Battlefield.Lands().ToList();
         }
+
+        return stats;
     }
 }
